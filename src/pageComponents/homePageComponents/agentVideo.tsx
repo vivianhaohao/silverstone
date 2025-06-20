03ff464551ca3c7584e3b2f8a219df7d9fda318f
ophelia <ophelia@opheliadeMacBook-Air.local>
2025-06-20 20:10:44 +0800

    delete one video

--- src/pageComponents/homePageComponents/agentVideo.tsx
+++ src/pageComponents/homePageComponents/agentVideo.tsx
@@ -7,6 +7,7 @@ export default function Agentvideo(){
                    playsInline
                    muted
                    preload="auto"
+                   loop
 
             >
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,6 @@
 import Head from '../pageComponents/homePageComponents/head.tsx'
 import Whyagent from '../pageComponents/homePageComponents/whyAgent.tsx'
 import Benefit from '../pageComponents/homePageComponents/benefits.tsx'
-import Tradingvideo from '../pageComponents/homePageComponents/tradingVideo.tsx'
 import Agents from '../pageComponents/homePageComponents/agents.tsx'
 import Agentvideo from '../pageComponents/homePageComponents/agentVideo.tsx'
 
@@ -10,7 +9,7 @@ export default function Home() {
         <>
             <Head/>
             <Whyagent/>
-            <Tradingvideo/>
+
             <Benefit />
             <Agentvideo/>
             <Agents/>
