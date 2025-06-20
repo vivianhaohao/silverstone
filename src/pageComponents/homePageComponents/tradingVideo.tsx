4ffec30e5ea1ba7f24078977f9c9b369462b4b94
ophelia <ophelia@opheliadeMacBook-Air.local>
2025-06-20 20:05:37 +0800

    reset video

--- src/pageComponents/homePageComponents/tradingVideo.tsx
+++ src/pageComponents/homePageComponents/tradingVideo.tsx
@@ -1,8 +1,8 @@
 export default function TradingVideo() {
     return (
         <div className="w-full  ">
-            <video src="/homePageResource/trade.mp4" className="w-full h-full object-cover "
-                autoPlay muted preload="auto"
+            <video src="/homePageResource/agent.mp4" className="w-full h-full object-cover "
+                   autoPlay muted preload="auto"
                 >
 
                 </video>
